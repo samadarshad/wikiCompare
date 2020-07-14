f6f442e3423eb9797a8f10115abd37e0d27544fd
Samad <samad@simprints.com>
2020-07-14 19:33:45 +0100

    removing

--- main7.py
+++ main7.py
@@ -8,21 +8,18 @@ entire_page = wiki.content.lower()
 
 entire_page = re.sub('[^a-z0-9 ]', '', entire_page)
 
+words = entire_page.split()
+
+counts = Counter(words)
+
 unwanted_words = ["the", "in", "of", "and", "to", "a", "on",
                   "for", "was", "as", "from", "with", "by", "that", "at",
                   "an", "after", "which", "not", "be", "had", "is", "it"]
 
-entire_page_cleaned = entire_page
-for i in unwanted_words :
-    entire_page_cleaned = entire_page_cleaned.replace(i, '')
-
-words = entire_page_cleaned.split()
-
-counts = Counter(words)
+for word in unwanted_words:
+    if word in counts:
+        del counts[word]
 
 for item, freq in counts.most_common(1000):
     print(item, '=', freq)
 
-# here we can see, we've done something unexpected, we've got rid of those particles, but its also messed with the origional data too
-# so lets take a different approach to filtering our data
-# instead, lets keep to this approach for the commas and equal signs, but for the removal of words, lets remove them directly from the sorted list rather than from the page
