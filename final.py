9eede8ad699e5201f030a4ab251fc7cb7bb1739d
Samad <samad@simprints.com>
2020-07-15 21:56:59 +0100

    very simple score algorithm

--- final.py
+++ final.py
@@ -34,14 +34,14 @@ def GetWordsFrequency(page_title):
 
     return {word : freq / total for (word, freq) in counts.most_common(topXwords)}
 
+def CalculateScoreBetween(a, b):
+    return min(a, b)
+
 def Compare(A, B):
     A_freq = GetWordsFrequency(A)
     B_freq = GetWordsFrequency(B)
 
-    comparedWords = { x : A_freq[x] * (
-            1 -
-             abs(B_freq[x]-A_freq[x])/max(B_freq[x], A_freq[x])
-             ) for x in A_freq if x in B_freq}
+    comparedWords = {x : CalculateScoreBetween(A_freq[x], B_freq[x]) for x in A_freq if x in B_freq}
     #note that this is an imperfect method for calculating the similarity between two frequency tables. A more suitable method would be a chi-squared test or something.
 
     score = sum(comparedWords.values())
