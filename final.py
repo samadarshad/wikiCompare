9b91058c3e8f09249461940ee9e9a2696db19b28
Samad <samad@simprints.com>
2020-07-15 18:15:58 +0100

    imperfect calculation

--- final.py
+++ final.py
@@ -2,16 +2,16 @@ import wikipedia
 from collections import Counter
 import re
 
-topXwords = 25
+topXwords = 250
 
 def GetWordsFrequency(page_title):
-    wiki = wikipedia.page(page_title)
+    page = wikipedia.page(page_title)
 
-    entire_page = wiki.content.lower()
+    page = page.content.lower()
 
-    entire_page = re.sub('[^a-z0-9 ]', '', entire_page)
+    page = re.sub('[^a-z0-9 ]', '', page)
 
-    words = entire_page.split()
+    words = page.split()
 
     counts = Counter(words)
 
@@ -23,6 +23,9 @@ def GetWordsFrequency(page_title):
         if word in counts:
             del counts[word]
 
+    # for item, freq in counts.most_common(topXwords):
+    #     print(item, '=', freq )
+
     total = sum([freq for (word, freq) in counts.most_common(topXwords)])
 
     return {word : freq / total for (word, freq) in counts.most_common(topXwords)}
@@ -31,14 +34,18 @@ def Compare(A, B):
     A_freq = GetWordsFrequency(A)
     B_freq = GetWordsFrequency(B)
 
-    vals = [A_freq[x] * (
+    comparedWords = { x : A_freq[x] * (
             1 -
-             abs(B_freq[x]-A_freq[x])/A_freq[x]
-             ) for x in A_freq if x in B_freq]
+             abs((B_freq[x]-A_freq[x])/A_freq[x])
+             ) for x in A_freq if x in B_freq}
+    #note that this is an imperfect method for calculating the similarity between two frequency tables. A more suitable method would be a chi-squared test or something.
 
-    print("The similarity score between " + A + " and " + B + " is " + str(sum(vals)))
+    score = sum(comparedWords.values())
 
-Compare("Barack Obama", "Cheese")
-Compare("Barack Obama", "Donald Trump")
-Compare("Barack Obama", "Hillary Clinton")
+    print("The similarity score between " + A + " and " + B + " is " + str(score))
+    print("The most common words are:")
+    print(Counter(comparedWords).most_common(10))
+
 Compare("Barack Obama", "Barack Obama")
+Compare("Barack Obama", "Donald Trump")
+Compare("Barack Obama", "Cheese")
