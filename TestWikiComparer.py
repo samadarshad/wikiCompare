8a129eeff43819668dd5529d465093d5752fae9e
samad <samadarshad@hotmail.co.uk>
2021-03-09 22:18:25 +0000

    wip

--- TestWikiComparer.py
+++ TestWikiComparer.py
@@ -1,20 +1,54 @@
 import unittest
 from wikiCompare.WikiComparer import WikiComparer
-
+from parameterized import parameterized
 
 class TestWikiComparer(unittest.TestCase):
     def test_get_words_from_wiki(self):
         wikiComparer = WikiComparer()
-        wikiComparer.get_page_from_wiki("Barack Obama")
+        page = wikiComparer.get_page_from_wiki("Barack Obama")
         expected_minimum_size_of_page = 100
-        self.assertGreater(len(wikiComparer.page), expected_minimum_size_of_page)
+        self.assertGreater(len(page), expected_minimum_size_of_page)
 
-    def test_get_words_from_page(self):
+    def test_remove_stop_words(self):
         wikiComparer = WikiComparer()
-        wikiComparer.page = "Dog Dog's Dogs dog dog, dog! dogging dogged"
-        wikiComparer.get_words_from_page()
-        expected_words = ["dog", "dog", "dog", "dog", "dog", "dog", "dog", "dog"]
-        self.assertEqual(wikiComparer.words, expected_words)
+        words = ["the", "dog", "was", "happy"]
+        expected_words = ["dog", "happy"]
+        clean_words = wikiComparer.remove_stop_words(words)
+        self.assertEqual(clean_words, expected_words)
+
+    @parameterized.expand([
+        ["obama", "obama president", ["obam", "presid"]], #stem removes the last bit of the word
+        ["dog", "Dog Dog's Dogs dog dog, dog! dogging dogged", ["dog", "dog", "dog", "dog", "dog", "dog", "dog", "dog"]]
+    ])
+    def test_get_words_from_page(self, name, page, expected_words):
+        wikiComparer = WikiComparer()
+        words = wikiComparer.get_words_from_page(page)
+        self.assertEqual(words, expected_words)
+
+    def test_remove_stop_words_a(self):
+        wikiComparer = WikiComparer()
+        words = ["the", "doga", "was", "happy"]
+        expected_words = ["doga", "happy"]
+        clean_words = wikiComparer.remove_stop_words(words)
+        self.assertEqual(clean_words, expected_words)
+
+    def test_get_words_by_count(self):
+        wikiComparer = WikiComparer()
+        words = ["a", "b", "a", "c"]
+        expected_word_counts = {"a": 2, "b": 1, "c": 1}
+        words_by_count = wikiComparer.get_words_by_count(words)
+        self.assertEqual(words_by_count, expected_word_counts)
+
+    def test_get_words_by_frequency(self):
+        wikiComparer = WikiComparer()
+        words_by_count = {"a": 2, "b": 1, "c": 1}
+        expected_word_frequencies = {"a": 0.5, "b": 0.25, "c": 0.25}
+        words_by_frequency = wikiComparer.get_words_by_frequency(words_by_count, 3)
+        self.assertEqual(words_by_frequency, expected_word_frequencies)
+
+    def test_get_words_frequency_of_page(self):
+        wikiComparer = WikiComparer()
+        words_frequency = wikiComparer.get_words_frequency_of_page("Barack Obama")
 
 if __name__ == '__main__':
     unittest.main()
--- WikiComparer.py
+++ WikiComparer.py
@@ -5,10 +5,16 @@ from collections import Counter
 import logging
 from functools import wraps
 from nltk.stem.lancaster import LancasterStemmer
+# import nltk
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt='%H:%M:%S', level=logging.INFO)
 
 TOP_X_WORDS = 100
+stop_words = set(stopwords.words('english'))
+
 
 def timing(function):
     @wraps(function)
@@ -18,44 +24,60 @@ def timing(function):
         end = time()
         logging.info("%r(%r, %r) took %2.2f secs" % (function.__name__, args, kwargs, end - start))
         return result
+
     return wrap
+
 
 class WikiComparer:
 
-    def __init__(self):
-        self.page = ""
-        self.words = []
-        self.wordsByCount = {}
-        self.wordsByFrequency = {}
+    def __init__(self, wikipedia=wikipedia):
+        self.wikipedia = wikipedia
 
     @timing
     def get_page_from_wiki(self, title):
         try:
             start = time()
-            self.page = wikipedia.page(title).content
+            page = wikipedia.page(title).content
             end = time()
             duration_secs = round((end - start), ndigits=2)
-            logging.info("First 100 characters of page: {}".format(self.page[:100]))
+            logging.info("First 100 characters of page: {}".format(page[:100]))
+            return page
 
         except wikipedia.PageError:
             raise Exception("Unrecognised page title, please be more specific.")
 
     @timing
-    def get_words_from_page(self):
-        self.page = self.page.lower()
-        self.page = re.sub('[^a-z0-9 ]', '', self.page)
-        logging.info("First 100 characters of cleaned page: {}".format(self.page[:100]))
+    def get_words_from_page(self, page):
+        page = page.lower()
+        page = re.sub('[^a-z0-9 ]', '', page)
+        logging.info("First 100 characters of cleaned page: {}".format(page[:100]))
 
-        self.words = self.page.split()
+        words = word_tokenize(page)
         stemmer = LancasterStemmer()
-        self.words = [stemmer.stem(word) for word in self.words]
-        logging.info("First 10 words: {}".format(self.words[:10]))
+        words = [stemmer.stem(word) for word in words]
+        logging.info("First 10 words: {}".format(words[:10]))
+        return words
 
-    def _removeStopWords(self):
-        self.words = ["abc", "def"]
+    @timing
+    def remove_stop_words(self, words):
+        return [word for word in words if word not in stop_words]
 
-    def getWordsByCount(self):
-        self.wordsByCount = {"abc": 1, "def": 1}
+    @timing
+    def get_words_by_count(self, words):
+        return Counter(words)
 
-    def getWordsByFrequencyOfTopXWords(self, top_x_words):
-        self.wordsByFrequency = {"abc": 0.5, "def": 0.5}
+    @timing
+    def get_words_by_frequency(self, words_by_count, top_x_words):
+        top_words = Counter(words_by_count).most_common(top_x_words)
+        total = sum([freq for (word, freq) in top_words])
+        return {word: freq / total for (word, freq) in top_words}
+
+    @timing
+    def get_words_frequency_of_page(self, title):
+        page = self.get_page_from_wiki(title)
+        words = self.get_words_from_page(page)
+        words = self.remove_stop_words(words)
+        words_by_count = self.get_words_by_count(words)
+        words_by_freq = self.get_words_by_frequency(words_by_count, TOP_X_WORDS)
+        logging.info("Top 10 words and their frequencies: {}".format(Counter(words_by_freq).most_common(10)))
+        return words_by_freq
